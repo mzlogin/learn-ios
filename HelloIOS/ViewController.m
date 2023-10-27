c089ef7dd919f9c8ce387686c6453147a29ebfcc
Zhuang Ma <chumpma@gmail.com>
2023-10-27 18:00:55 +0800

    update

--- HelloIOS/ViewController.m
+++ HelloIOS/ViewController.m
@@ -7,7 +7,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate, UITextViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UILabel *helloLabel;
+@property (weak, nonatomic) IBOutlet UITextField *bookNameTextField;
+
+- (IBAction)onClick:(id)sender;
 
 @end
 
@@ -18,5 +23,32 @@
     // Do any additional setup after loading the view.
 }
 
+
+- (IBAction)onClick:(id)sender {
+    self.bookName = self.bookNameTextField.text;
+    NSString *nameString = self.bookName;
+    if ([nameString length] == 0) {
+        nameString = @"World";
+    }
+    NSString *greeting = [[NSString alloc] initWithFormat:@"Hello, %@!", nameString];
+    
+    self.helloLabel.text = greeting;
+}
+
+// 通过委托来放弃“第一响应者”
+#pragma mark - UITextField Delegate Method
+- (BOOL) textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
+
+#pragma mark - UITextView Delegate Method
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+    if ([text isEqualToString:@"\n"]) {
+        [textView resignFirstResponder];
+        return NO;
+    }
+    return YES;
+}
 
 @end
